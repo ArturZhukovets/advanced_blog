78c787092fc39ccd796bd8b91736a667723173a3
Zhukovec Artur <bnlvdn7483@gmail.com>
2022-06-10 01:26:57 +0300

    append new many-to-many model (Tags). work with templates

--- blogengine/blog/models.py
+++ blogengine/blog/models.py
@@ -8,6 +8,7 @@ class Post(models.Model):
     slug = models.SlugField(max_length=150, unique=True)
     body = models.TextField(blank=True, db_index=True)
     date_pub = models.DateTimeField(auto_now_add=True)
+    tags = models.ManyToManyField('Tag', blank=True, related_name='posts')  # related name default = post_set
 
     def get_absolute_url(self):
         """ reverse is generate link"""
@@ -15,3 +16,14 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50, unique=True)
+
+    def get_absolute_url(self):
+        return reverse('tag_detail_url', kwargs={'slug': self.slug})
+
+    def __str__(self):
+        return f'{self.title}'
